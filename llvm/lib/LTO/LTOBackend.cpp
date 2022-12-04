16b773eb23831af4675687f1e9a878bc4d4fe98b
James Smith <jamesmth@proton.me>
2022-12-04 15:59:27 +0100

    Print out error message on plugin loading failure

--- llvm/lib/LTO/LTOBackend.cpp
+++ llvm/lib/LTO/LTOBackend.cpp
@@ -193,7 +193,8 @@ static void RegisterPassPlugins(ArrayRef<std::string> PassPlugins,
     auto PassPlugin = PassPlugin::Load(PluginFN);
     if (!PassPlugin) {
       errs() << "Failed to load passes from '" << PluginFN
-             << "'. Request ignored.\n";
+             << "' (" << PassPlugin.takeError()
+             << "). Request ignored.\n";
       continue;
     }
 
